45bb70c1731e96ea4ead2745148ea93db2bda626
Rahul Paul <168732280+rahulpal194@users.noreply.github.com>
2024-10-22 18:13:38 +0600

    add logo slider, features, description

--- js/script.js
+++ js/script.js
@@ -8,3 +8,42 @@ opensidebar.addEventListener('click', function (){
 closesidebar.addEventListener('click', function (){
     sidebar.classList.remove('sidebar-active')
 })
+
+
+const slidesContainers = document.querySelectorAll('.slides');
+let isDragging = false;
+let startX;
+let scrollLeft;
+
+slidesContainers.forEach((slide) => {
+    slide.addEventListener('mousedown', (e) => {
+        isDragging = true;
+        startX = e.pageX - slide.offsetLeft; // Get the initial mouse position
+        scrollLeft = slide.scrollLeft; // Store the current scroll position
+        slide.style.cursor = 'grabbing'; // Change cursor to grabbing
+    });
+
+    slide.addEventListener('mouseleave', () => {
+        isDragging = false;
+        slide.style.cursor = 'grab'; // Reset cursor
+    });
+
+    slide.addEventListener('mouseup', () => {
+        isDragging = false;
+        slide.style.cursor = 'grab'; // Reset cursor
+    });
+
+    slide.addEventListener('mousemove', (e) => {
+        if (!isDragging) return; // Stop if not dragging
+        e.preventDefault(); // Prevent text selection
+        const x = e.pageX - slide.offsetLeft; // Current mouse position
+        const walk = (x - startX) * 2; // Calculate how far to scroll
+        slide.scrollLeft = scrollLeft - walk; // Update scroll position
+    });
+});
+
+
+
+
+
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,10 +11,23 @@ module.exports = {
         "secondary": "var(--secondary)",
       },
       backgroundImage:{
+        "gradient-to-250":"linear-gradient(250deg, var(--secondary),var(--primary))",
         herobg:" url('/images/hero/background.png')"
       },
+      boxShadow:{
+        feature:"0px 6px 48px 0px rgba(87, 89, 102, 0.04)"
+      },
       dropShadow:{
-        hero:'82px 20px 128px rgba(23, 30, 54, 0.16)'
+        hero:'6px 31px 11px rgba(23, 30, 54, 0.16)'
+      },
+      animation: {
+        'infinite-scroll': 'infinite-scroll 20s linear infinite',
+      },
+      keyframes: {
+        'infinite-scroll': {
+          from: { transform: 'translateX(0)' },
+          to: { transform: 'translateX(-100%)' },
+        }
       }
       
     }
